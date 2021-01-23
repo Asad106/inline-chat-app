2d6956f8ff1935fbf26aee9a8b823bd9ebdb3664
Asad106 <asadalkhair1994@gmail.com>
2021-01-23 00:15:16 -0800

    chat application final release

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,59 +1,57 @@
-import React from 'react';
-import { NavLink, Link } from 'react-router-dom';
-import './style.css';
-import { useSelector, useDispatch } from 'react-redux';
-import { logout } from '../../actions';
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import "./style.css";
+import { useSelector, useDispatch } from "react-redux";
+import { logout } from "../../actions";
 
 /**
-* @author
-* @function Header
-**/
+ * @author
+ * @function Header
+ **/
 
 const Header = (props) => {
-
-  const auth = useSelector(state => state.auth);
+  const auth = useSelector((state) => state.auth);
   const dispatch = useDispatch();
 
   // const logout = () => {
   //   dispatch(logout())
   // }
 
-  return(
+  return (
     <header className="header">
-        <div style={{display: 'flex'}}>
-          <div className="logo">Web Messenger</div>
-            
-            {
-              !auth.authenticated ? 
-              <ul className="leftMenu">
-                <li><NavLink to={'/login'}>Login</NavLink></li>
-                <li><NavLink to={'/signup'}>Sign up</NavLink></li>
-              </ul> : null
-            }
-              
+      <div style={{ display: "flex" }}>
+        <div className="logo">Inline Chat App</div>
 
-            
-        </div>
-          <div style={{margin: '20px 0', color: '#fff', fontWeight: 'bold'}}>
-            {auth.authenticated ? `Hi ${auth.firstName} ${auth.lastName}` : ''}
-          </div>
-        <ul className="menu">
-
-            {
-              auth.authenticated ?
-              <li>
-                <Link to={'#'} onClick={() => {
-                  dispatch(logout(auth.uid))
-                }}>Logout</Link>
-            </li> : null
-            }
-          
-            
-             
-        </ul>
+        {!auth.authenticated ? (
+          <ul className="leftMenu">
+            <li>
+              <NavLink to={"/login"}>Login</NavLink>
+            </li>
+            <li>
+              <NavLink to={"/signup"}>Sign up</NavLink>
+            </li>
+          </ul>
+        ) : null}
+      </div>
+      <div style={{ margin: "20px 0", color: "#fff", fontWeight: "bold" }}>
+        {auth.authenticated ? `Hi ${auth.firstName} ${auth.lastName}` : ""}
+      </div>
+      <ul className="menu">
+        {auth.authenticated ? (
+          <li>
+            <Link
+              to={"#"}
+              onClick={() => {
+                dispatch(logout(auth.uid));
+              }}
+            >
+              Logout
+            </Link>
+          </li>
+        ) : null}
+      </ul>
     </header>
-   )
+  );
+};
 
- }
-
-export default Header
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import firebase from 'firebase';
-import { Provider } from 'react-redux';
-import store from './store';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import firebase from "firebase";
+import { Provider } from "react-redux";
+import store from "./store";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBF8co3-j6lq7qGI9b8s0U1YYEIwqXb4_0",
-  authDomain: "web-messenger-4eb99.firebaseapp.com",
-  databaseURL: "https://web-messenger-4eb99.firebaseio.com",
-  projectId: "web-messenger-4eb99",
-  storageBucket: "web-messenger-4eb99.appspot.com",
-  messagingSenderId: "252569616509",
-  appId: "1:252569616509:web:2bbb4c394e9d608f147114",
-  measurementId: "G-VXYE76S2G7"
+  apiKey: "AIzaSyBWlJTXp7Q4Tye5ms_SJNpJ1FzVttUfmXw",
+  authDomain: "chat-app-b659d.firebaseapp.com",
+  projectId: "chat-app-b659d",
+  storageBucket: "chat-app-b659d.appspot.com",
+  messagingSenderId: "830833753710",
+  appId: "1:830833753710:web:f1ffbf94d2030c0783f07f",
 };
-
-
 firebase.initializeApp(firebaseConfig);
 
 window.store = store;
@@ -31,7 +26,7 @@ ReactDOM.render(
       <App />
     </React.StrictMode>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
